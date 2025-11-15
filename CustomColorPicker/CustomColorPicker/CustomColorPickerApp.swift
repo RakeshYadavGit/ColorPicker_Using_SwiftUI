58351d9eb74d17e15c5dce97f8ee9a0a63c3536c
Rakesh Yadav <rryadav142001@gmail.com>
2025-11-15 10:52:03 +0530

    initial commit

---
+++ CustomColorPicker/CustomColorPicker/CustomColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomColorPickerApp.swift
+//  CustomColorPicker
+//
+//  Created by Rakesh Yadav on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
